d1cfa53d9f9155768339ac50fd9a45d1ba1562b5
nowitworks <joao.pinto.silveira@gmail.com>
2018-01-04 10:14:26 +0100

    Change reputation mechanism with opinions and fix blacklist logic

--- reputation.go
+++ reputation.go
@@ -1,70 +1,144 @@
 package main
 
-type ReputationTable map[string]*Reputation
+import "strconv"
+
+type ReputationMap map[string]*Reputation
+
+type ReputationTable struct {
+	Reputations ReputationMap
+	Threshold   int
+}
+
+func NewReputationTable(threshold int) ReputationTable {
+	return ReputationTable{
+		Reputations: make(ReputationMap),
+		Threshold:   threshold,
+	}
+}
 
 type Reputation struct {
 	Value int
-	IsOld bool
+	Peer  string
+	//IsOld bool
 }
 
-func newReputation() *Reputation {
-	return &Reputation{
-		IsOld: true,
-		Value: 0,
+type RepOpinions map[string]int
+
+func (opinions RepOpinions) Suspect(peer string) {
+	opinions[peer] = -1
+}
+
+func (opinions RepOpinions) Trust(peer string) {
+	// Opinion can only change from trusting to suspecting,
+	// not the other way around
+	if opinions[peer] != -1 {
+		opinions[peer] = 1
 	}
 }
 
-func (repTable ReputationTable) NextRound() {
-	for _, rep := range repTable {
-		rep.IsOld = true
+func invalidOpinion(opinions RepOpinions) bool {
+	for _, rep := range opinions {
+		if rep != 1 && rep != -1 {
+			return true
+		}
 	}
+	return false
 }
 
-func (repTable ReputationTable) Trust(peer string) {
-	_, exists := repTable[peer]
+/*
+Input a slice with all the received opinions.
+They will all be added and will update the reputation table.
+Reputations above 0 are not allowed so after adding everything,
+all the reputations above 0 are set to 0.
+*/
+func (repTable ReputationTable) AddReputations(allOpinions []RepOpinions) {
+	for _, peerOpinions := range allOpinions {
+
+		// If a peer has an invalid opinion of another peer
+		// none of its opinions will be taken into account
+		// TODO should this peer be blacklisted???
+		if invalidOpinion(peerOpinions) {
+			continue
+		}
+
+		for peer, rep := range peerOpinions {
+			repTable.tempUpdateRep(peer, rep)
+		}
+	}
+
+	repTable.regularize()
+}
+
+func (repTable ReputationTable) tempUpdateRep(peer string, rep int) {
+	_, exists := repTable.Reputations[peer]
 
 	if !exists {
-		repTable[peer] = newReputation()
+		repTable.Reputations[peer] = &Reputation{
+			Value: 0,
+			Peer:  peer,
+		}
 	}
 
-	if repTable[peer].IsOld {
-		repTable[peer].Value += 1
-		repTable[peer].IsOld = false
+	repTable.Reputations[peer].Value += rep
+}
+
+func (repTable ReputationTable) regularize() {
+	for peer, rep := range repTable.Reputations {
+		if rep.Value > 0 {
+			repTable.Reputations[peer].Value = 0
+		}
 	}
 }
 
-func (repTable ReputationTable) Suspect(peer string) {
-	_, exists := repTable[peer]
+func (repTable ReputationTable) String() string {
+	str := "Threshold: " + strconv.Itoa(repTable.Threshold) + "\n"
 
-	if !exists {
-		repTable[peer] = newReputation()
+	for peer, rep := range repTable.Reputations {
+		str += peer + "\t...\t" + strconv.Itoa(rep.Value) + "\n"
 	}
 
-	if repTable[peer].IsOld {
-		repTable[peer].Value -= 1
-		repTable[peer].IsOld = false
-	}
+	return str
+}
+
+type ReputationPacket struct {
+	Opinions RepOpinions
+
+	//TODO change
+	PollID    uint64 //TODO change according to representation
+	Signature []byte //TODO change according to representation
 }
 
 type Blacklist map[string]bool
 
-func (bl Blacklist) isBlacklisted(peer string) bool {
-	return bl[peer]
+func (bList Blacklist) IsBlacklisted(peer string) bool {
+	return bList[peer]
 }
 
-func (bl Blacklist) blacklist(peer string) {
-	bl[peer] = true
+func (bList Blacklist) add(peer string) {
+	bList[peer] = true
 }
 
-func (repTable ReputationTable) updateBlacklist(bList Blacklist) {
-	//TODO threshold
-	threshold := -12 //THIS IS NOT OKAY
-
-	for peer, rep := range repTable {
-		if rep.Value < threshold {
-			bList.blacklist(peer)
+// This should be used after having a final reputation table considering the opinions of the other peers
+func (bList Blacklist) UpdateBlacklist(repTable ReputationTable) {
+	for peer, rep := range repTable.Reputations {
+		if rep.Value < repTable.Threshold {
+			bList.add(peer)
 		}
 	}
+}
+
+func (bList Blacklist) String() string {
+	str := "Peer\t\tBlacklisted?\n"
+	for peer, status := range bList {
+		str += peer + "\t\t"
+		if status {
+			str += "yes\n"
+		} else {
+			str += "no\n"
+		}
+	}
+
+	return str
 }
 
 func updateReputations() {
--- reputation_test.go
+++ reputation_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"testing"
-	"net"
 )
 
 /*func TestMain(m *testing.M) {
@@ -12,7 +11,7 @@ import (
 	os.Exit(retCode)
 }*/
 
-func TestSuspect(t *testing.T) {
+/*func TestSuspect(t *testing.T) {
 
 	repTab := make(ReputationTable)
 
@@ -162,22 +161,84 @@ func TestFiveRounds(t *testing.T) {
 		t.Error("Didn't update reputation status")
 	}
 
-}
+}*/
 
 func TestBlacklist(t *testing.T) {
 	bl := make(Blacklist)
 	peerA := "peerA"
 	peerB := "peerB"
 
-	bl.blacklist(peerA)
+	bl.add(peerA)
 
-	if !bl.isBlacklisted(peerA) {
+	if !bl.IsBlacklisted(peerA) {
 		t.Error("Didn't blacklist peer but should have")
 	}
 
-	if bl.isBlacklisted(peerB) {
+	if bl.IsBlacklisted(peerB) {
 		t.Error("Blacklisted peer but shouldn't have")
 	}
 
 }
 
+func TestOpinions(t *testing.T) {
+	repTabA := NewReputationTable(-3)
+	repTabB := NewReputationTable(-3)
+	repTabC := NewReputationTable(-3)
+
+	peerA := "peerA"
+	peerB := "peerB"
+	peerC := "peerC"
+
+	// round 1
+	opinionsA := make(RepOpinions)
+	opinionsB := make(RepOpinions)
+	opinionsC := make(RepOpinions)
+
+	opinionsA.Trust(peerA)
+	opinionsA.Trust(peerB)
+	opinionsA.Suspect(peerC)
+
+	opinionsB.Trust(peerA)
+	opinionsB.Trust(peerB)
+	opinionsB.Suspect(peerC)
+
+	opinionsC.Suspect(peerA)
+	opinionsC.Suspect(peerB)
+	opinionsC.Trust(peerC)
+
+	listOpinions := append(make([]RepOpinions, 0), opinionsA, opinionsB, opinionsC)
+
+	repTabA.AddReputations(listOpinions)
+	repTabB.AddReputations(listOpinions)
+	repTabC.AddReputations(listOpinions)
+
+	repTabA.AddReputations(listOpinions)
+	repTabB.AddReputations(listOpinions)
+	repTabC.AddReputations(listOpinions)
+
+	repTabA.AddReputations(listOpinions)
+	repTabB.AddReputations(listOpinions)
+	repTabC.AddReputations(listOpinions)
+
+	repTabA.AddReputations(listOpinions)
+	repTabB.AddReputations(listOpinions)
+	repTabC.AddReputations(listOpinions)
+
+	//fmt.Println("Table A\n", repTabA)
+	//fmt.Println("\nTable B\n", repTabB)
+	//fmt.Println("\nTable C\n", repTabC)
+
+	bl := make(Blacklist)
+
+	bl.UpdateBlacklist(repTabA)
+
+	//fmt.Println(bl)
+
+	if bl.IsBlacklisted(peerA) || bl.IsBlacklisted(peerB) {
+		t.Error("Blacklisted peer but shouldn't have")
+	}
+
+	if !bl.IsBlacklisted(peerC) {
+		t.Error("Didn't blacklist peer but should have")
+	}
+}
